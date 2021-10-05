8c4b89049a11676f7942bbef51f3d6bfce108d91
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2021-10-06 02:04:34 +0700

    Added scripts for SBB 2021

---
+++ sbb_2021/sbb_pract_1.R
@@ -0,0 +1,169 @@
+---
+title: "Практикум по Компьютерной статистической геномике. Часть 1: Работа с фенотипическими данными."
+author: "Содбо Шарапов, Елгаева Елизавета"
+date: "6 октября 2021 г."
+output: html_document
+---
+  
+  ```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+# План практикума
+
+#Практическое занятие состоит из трех частей:
+  
+#1. Работа с фенотипическими данными
+#2. Работа с данными генотипов, контроль качества
+#3. Проведение GWAS
+
+## Загрузка программных пакетов
+
+```{r}
+library(GenABEL)
+library(GenABEL.data)
+```
+
+## Загрузка тестового набора данных
+
+```{r}
+data("ge03d2")
+```
+
+# Общая структура данных
+
+```{r}
+str(ge03d2)
+phdata(ge03d2) # то же самое, что и ge03d2@phdata
+ge03d2@phdata$dm2
+
+gtdata(ge03d2) #  то же самое, что и summary(ge03d2@gtdata)
+ge03d2@gtdata@chromosome
+
+```
+
+# Работа с данными фенотипов
+
+```{r}
+pheno <- phdata(ge03d2)
+head(pheno)
+```
+
+## Описание колонок в pheno:
+
+# 1. ID: id code of sample 
+# 2. sex: sex, '0' is female, '1' is male
+# 3. age: age, in years
+# 4. dm2: diabetes mellitus type 2, '0' is unaffected, '1' is affected
+# 5. height: height, in cm
+# 6. wieght: wieght, in kg
+# 7. diet: following the diet, '0' is false, '1' is true
+# 8. bmi: BMI, body mass index, in $cm/kg^2$
+
+## Cтруктура фенотипических данных
+
+```{r}
+summary(pheno)
+```
+
+## Для категориальных признаков применим функцию **table**
+
+```{r}
+table(pheno$sex)
+table(pheno$dm2)
+table(pheno$diet)
+```
+
+## Проверка распределений признаков для количественных признаков
+
+```{r}
+hist(pheno$age, main='Histogram of age', xlab = 'Age')
+hist(pheno$height, main='Histogram of height', xlab = 'Height')
+hist(pheno$weight, main='Histogram of weight', xlab = 'Weight')
+hist(pheno$bmi, main='Histogram of bmi', xlab = 'BMI')
+```
+
+## Проверка на наормальность распределения с помощью Шапиро теста
+## Нулевая гипотеза - распределение является нормальным.
+## Если p-value <= 0.05/n, где n - количество тестов, то распределение ненормальное
+ 
+```{r}
+shapiro.test(pheno$height)
+shapiro.test(pheno$weight)
+shapiro.test(pheno$bmi)
+```
+
+## Для приведения к нормальному распределению можно использовать логарифмирование
+
+```{r}
+hist(log(pheno$bmi), main='Histogram of logBMI', xlab = 'logBMI')
+shapiro.test(log(pheno$bmi))
+```
+
+# Или ранговую трансформацию
+
+```{r}
+hist(rntransform(pheno$bmi), main='Histogram of BMI after rank transformation', xlab = 'BMI, rank transformed')
+shapiro.test(rntransform(pheno$bmi))
+```
+
+## Оценка корреляционной структуры для определения ковариат 
+## Можно использовать коэффициент корреляции Пирсона и/или провести анализ линейной регрессии
+
+```{r}
+cor_matrix <- cor(pheno[,c('dm2','sex','age','height','weight','bmi','diet')], use='complete.obs')
+cor_matrix
+```
+## Визуализация с помощью пакета *corplot*
+
+```{r}
+library(corrplot)
+corrplot(cor_matrix)
+```
+
+## Проведем линейную регрессию на ковариаты
+
+```{r}
+dm2_weight <- glm(dm2~weight,data = pheno,family = 'binomial')
+summary(dm2_weight)
+```
+
+```{r}
+dm2_bmi <- glm(dm2~bmi,data = pheno,family = 'binomial')
+summary(dm2_bmi)
+```
+
+```{r}
+dm2_sex <- glm(dm2~sex,data = pheno,family = 'binomial')
+summary(dm2_sex)
+```
+
+# beta для пола 0.4465, что соответствует OR=exp(0.4465)=1.563. Проверим по таблице 2x2:
+```{r}
+table(pheno$dm2,pheno$sex)
+(275*253)/(210*212)
+````
+
+```{r}
+dm2_cov <- glm(dm2~sex + age,data = pheno,family = 'binomial')
+summary(dm2_cov)
+```
+
+## Подготовка фенотипических данных могла бы выглядеть так:
+
+```{r}
+table(is.na(pheno))
+table(is.na(pheno$id))
+table(is.na(pheno$sex))
+table(is.na(pheno$age))
+table(is.na(pheno$dm2))
+table(is.na(pheno$height))
+table(is.na(pheno$weight))
+table(is.na(pheno$diet))
+table(is.na(pheno$bmi))
+
+dm2_adj <- residuals(glm(dm2~sex + age,data = pheno,family = 'binomial'))
+shapiro.test(dm2_adj)
+shapiro.test(rntransform(dm2_adj))
+dm2_adj_norm <- rntransform(dm2_adj)
+```
---
+++ sbb_2021/sbb_pract_2.R
@@ -0,0 +1,290 @@
+---
+title: "Практикум по Компьютерной статистической геномике. Часть 2: Работа с данными генотипов, контроль качества."
+author: "Содбо Шарапов, Елгаева Елизавета"
+date: "6 октября 2021 г."
+output: html_document
+---
+  
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+# План практикума
+
+#Практическое занятие состоит из трех частей:
+
+#1. Работа с фенотипическими данными
+#2. Работа с данными генотипов, контроль качества
+#3. Проведение GWAS
+
+## Загрузка программных пакетов
+
+```{r}
+library(GenABEL)
+library(GenABEL.data)
+```
+
+## Загрузка тестового набора данных
+
+```{r}
+data("ge03d2")
+```
+
+# Общая структура данных
+
+```{r}
+str(ge03d2)
+gtdata(ge03d2) #  то же самое, что и summary(ge03d2@gtdata)
+ge03d2@gtdata@chromosome
+
+```
+
+# Работа с генотипами
+
+```{r}
+nids(ge03d2)
+nsnps(ge03d2)
+chromosome(ge03d2)
+coding(ge03d2)
+as.character(gtdata(ge03d2[1:5, 1:5]))
+as.numeric(gtdata(ge03d2[1:5, 1:5]))
+effallele(ge03d2@gtdata)
+refallele(ge03d2@gtdata)
+```
+
+## Информация по SNP
+  
+```{r}
+snp_info <- summary(gtdata(ge03d2))
+head(snp_info)
+```
+
+## Описание колонок:
+  
+# 1. Q.2 - frequency of allele 2 (A2)
+# 2. Pexact - p-value of HWE test
+# 3. P.11, P.12, P.22 - number of genotypes
+
+## Распределение количества SNP по хромосомам
+  
+```{r}
+table(chromosome(ge03d2))
+```
+
+## Информация по отдельным SNP
+
+```{r}
+snp_info['rs6212914',]
+```
+
+## Распределение генотипов для отдельного SNP
+
+```{r}
+table(as.character(gtdata(ge03d2[,'rs6212914'])))
+```
+
+## Оценка частоты эффекторного аллеля
+
+```{r}
+(snp_info['rs6212914','P.12'] + snp_info['rs6212914','P.22'] * 2) / snp_info['rs6212914','NoMeasured'] / 2
+mean(as.numeric(gtdata(ge03d2[,'rs6212914'])), na.rm=TRUE) / 2
+```
+## Проверка на равновесие Харди-Вайнберга (HWE test)
+
+```{r}
+HWE.show(ge03d2[,'rs6212914'])
+```
+
+## Статистика по отдельному SNP
+
+# Сперва добавим колонку EAF (effective allele frequency). Та же информация есть в 'Q.2',
+# но мы попробуем посчитать ее самостоятельно
+
+```{r}
+snp_info$EAF <- (snp_info[,'P.12'] + snp_info[,'P.22'] * 2) / snp_info[,'NoMeasured'] / 2
+head(snp_info)
+```
+
+# Распределение EAF
+
+```{r}
+hist(snp_info$EAF, breaks=50)
+```
+
+# Множество SNP c EAF меньше 0.05
+
+# Проверим процент генотипирования по образцам (call rate)
+  
+```{r}
+hist(snp_info$CallRate, breaks=50)
+```
+
+# Проведем HWE test
+  
+```{r}
+catable(snp_info$Pexact, c(0.05/nsnps(ge03d2), 0.01, 0.05, 0.1), cum=TRUE)
+```
+
+# Отдельно для контрольной выборки
+  
+```{r}
+control_ids <- idnames(ge03d2)[phdata(ge03d2)$dm2==0]
+catable(summary(gtdata(ge03d2[control_ids,]))$Pexact, c(0.05/nsnps(ge03d2), 0.01, 0.05, 0.1), cum=TRUE)
+```
+# В контрольной выборке меньше SNP отклоняются от равновесия
+
+
+## Теперь посмотрим, как проверить статистику по обазцу
+
+# Статистика по ID
+```{r}
+idsummary <- perid.summary(ge03d2)
+head(idsummary)
+```
+
+# Описание колонок:
+  
+# 1. NoMeasured - number of measured SNPs
+# 2. NoPoly - number of polymorfic SNPs (SNPs with minor allele frequency > 0)
+# 3. Het - heterozigosity rate
+
+# Проверим процент генотипирования по SNP
+
+```{r}
+hist(idsummary$CallPP, breaks=100)
+```
+
+# Проверим гетерозиготность (heterozigosity rate)
+
+```{r}
+hist(idsummary$Het, breaks=100)
+```
+
+# Несколько образцов с высокой гетерозиготностью, что говорит о возможной контаминации образцов
+
+```{r}
+descriptives.marker(ge03d2)
+descriptives.marker(ge03d2, ids=control_ids)
+```
+
+## Контроль качества данных генотипирования
+
+# Базовые этапы QC:
+
+# 1. Проверка несоответствий по полу
+# a. X-гетерозиготные мужчины
+# b. Y-гетерозиготные женщины
+ 
+# 2. Проверка по проценту генотипирования по SNP и поиск аутлаеров по гетерозиготности
+# a. ID с низким call rate (<95%, 97% or 99%)
+# b. ID с экстримальными значениями гетерозиготности
+#  высокие - контаминация ДНК 
+#  низкие - инбридинг
+
+# 3. Поиск дубликатов и родственников
+# a. Оценка матрицы родства (kinship matrix)
+#  IBS = 1 -> монозиготные близнецы или дубликаты
+#  IBS > 0.125 -> близкие родственники
+
+# 4. Определение образцов другой этнической принадлежности
+# a. PCA анализ с использованием матрицы родства
+# b. 1000 Genomes project в качестве референса
+
+# 5. Удаление всех образцов, не прошедших QC
+ 
+# 6. Проверка SNP на процент генотиппирования по образцам
+# a. SNP с call rate меньше 97% (95%, 99%)
+
+# 7. Проверка частот аллелей
+# MAF > 0.01
+
+# 8. Проверка на равновесие Харди-Вайнберга
+ 
+# 9. Удаление всех SNP, не прошедших QC
+
+
+# Начнем QC c функцией **check.marker**
+
+```{r}
+qc1 <- check.marker(ge03d2, p.level = 0, maf = 0.01, perid.call = 0.97, callrate = 0.97)
+```
+
+# Определим ID, которые будут проверены на HWE
+
+```{r}
+hweids <- intersect(control_ids, qc1$idok)
+qc2 <- check.marker(ge03d2, 
+p.level = 1e-6, 
+maf = 0.01, 
+perid.call = 0.97, 
+callrate = 0.97,
+hweidsubset = hweids)
+```
+
+# QC окончен, сохраним результат
+
+```{r}
+ge03d2clean <- ge03d2[qc2$idok,qc2$snpok]
+```
+
+## Проверим данные на стартификациюe
+
+# Используем алгоритм, предложенный Price at al.
+# Сначала вычислим матрицу родства по аутосомным маркерам:
+  
+```{r}
+gkin <- ibs(ge03d2clean[, autosomal(ge03d2clean)], weight="freq")
+gkin[1:5, 1:5]
+```
+
+# Числа под диагональю отражают оценки геномного родства
+# (’genomic kinship’ или ’genome-wide IBS’), сисла по диагонали соответствуют
+# 0.5 + гомозиготность генома, наддиагональные элементы говорят о том,
+# сколько SNP было генотипировано для обоих объектов (IBD оценивается по этим SNP)
+
+# Переведем матрицу в матрицу расстояний
+
+```{r}
+data.dist <- as.dist(0.5 - gkin)
+```
+
+# Многомерное шкалирование коэффициентов (Classical Multidimensional Scaling)
+
+```{r}
+data.mds <- cmdscale(data.dist)
+```
+
+# Визуализация результата
+
+```{r}
+plot(data.mds)
+```
+
+# Каждая точка на графике - это отдельный индивид, 2D расстояние между ними 
+# подстроено так, чтобы быть максимально близким к расстоянию из исходной матрицы расстояний
+# Данные явно кластеризуются в две группы 
+# Получим id образцов из второго кластера и исключим их, затем повторим QC
+
+```{r}
+clean_ids <- rownames(data.mds)[data.mds[,1]<0.1]
+controls_clean <- intersect(clean_ids,idnames(ge03d2clean)[phdata(ge03d2clean)$dm2==0])
+qc3 <- check.marker(ge03d2[clean_ids,], 
+p.level = 1e-6, 
+maf = 0.01, 
+perid.call = 0.97, 
+callrate = 0.97,
+hweidsubset = controls_clean)
+ge03d2clean <- ge03d2[qc3$idok,qc3$snpok]
+```
+
+## Сохраним данные для GWAS
+
+```{r}
+save(ge03d2clean, file='.../ge03d2clean.RData')
+```
+## Литература: 
+
+# Aulchenko, Yurii S., Karssen, Lennart C., & The GenABEL project developers. (2015). The GenABEL Tutorial. Zenodo. http://doi.org/10.5281/zenodo.19738
+ 
+# Anderson, C. A., Pettersson, F. H., Clarke, G. M., Cardon, L. R., Morris, A. P., & Zondervan, K. T. (2010). Data quality control in genetic case-control association studies. Nature Protocols, 5(9), 1564–73. http://doi.org/10.1038/nprot.2010.116
+
